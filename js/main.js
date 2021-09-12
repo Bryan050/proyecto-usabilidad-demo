0927cf7cf119c16829393da8ff8d3237c995012d
Bryan050 <bryanchicaiza14@gmail.com>
2021-09-11 21:34:46 -0500

    Integración de búsqueda instantánea

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
-$('#logo').click(function (){
+$('#logo').click(function () {
 	window.location.href = 'index.html'
 });
 
-$('.caja').click(function (){
+$('.caja').click(function () {
 	window.location.href = 'resultado.html'
+});
+
+$(document).ready(function () {
+	$.ajaxSetup({ cache: false });
+	$('#txtBuscar').keyup(function (e) {
+		var searchField = $('#txtBuscar').val();
+		if (e.key === "Escape" || searchField === "") {
+			$("#result").html('');
+		} else {
+			$('#result').html('');
+			$('#state').val('');
+			var expression = new RegExp(searchField, "i");
+			$.getJSON('data.json', function (data) {
+				$.each(data, function (key, value) {
+					if (value.name.search(expression) != -1 || value.location.search(expression) != -1) {
+						$('#result').append('<li class="list-group-item link-class">' + '<span class="val-result">'+value.location+'</span>' + '</li>');
+					}
+				});
+			});
+		}
+	});
+
+	$('#result').on('click', 'li', function () {
+		var click_text = $(this).text().split('|');
+		$('#txtBuscar').val($.trim(click_text[0]));
+		$("#result").html('');
+	});
+	$(document).click(function () {
+		$("#result").html('');
+	});
 });
